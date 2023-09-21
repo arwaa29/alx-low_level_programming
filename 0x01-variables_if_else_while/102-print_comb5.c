a259e8f0b163ff5eb73143f5081eeae58093d2a4
arwaa29 <arwahesham73@gmail.com>
2023-09-21 20:01:53 +0300

    done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all possible combination of two two digits from 0 to 99
+ *              combination of numbers must be separated by (,)
+ *              using putchar function.
+ * Return: always 0 (success).
+*/
+
+int main(void)
+{
+	int first_digit = 0, second_digit;
+
+	while (first_digit <= 99)
+	{
+		second_digit = first_digit;
+		while (second_digit <= 99)
+		{
+			if (second_digit != first_digit)
+			{
+				putchar((first_digit / 10) + 48);
+				putchar((first_digit % 10) + 48);
+				putchar(' ');
+				putchar((second_digit / 10) + 48);
+				putchar((second_digit % 10) + 48);
+
+				if (first_digit != 98, second_digit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			second_digit++;
+		}
+		first_digit++;
+	}
+	putchar('\n');
+	return (0);
+}
+
