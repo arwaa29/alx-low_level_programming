6ce20b3ccf57e0f783aee228731222532cf54614
arwaa29 <arwahesham73@gmail.com>
2023-09-26 01:33:43 +0300

    alx 2

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the _ character
+ *     should be printed
+*/
+
+void print_line(int n)
+{
+	int lnChr;
+
+	if (n <= 0)
+		_puchar('\n');
+	else
+	{
+		for (lnChr = 1; lnChr <= n; lnChr++)
+			-putchar('_');
+		_putchar('\n');
+	}
+}
