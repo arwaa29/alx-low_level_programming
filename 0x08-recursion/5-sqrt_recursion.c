82c2e80a0d6454c22300bb284132638343bee9f9
arwaa29 <arwahesham73@gmail.com>
2023-09-29 17:27:13 +0300

    alx 8

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
  * _sqrt_recursion - find natural square root
  * @n: int
  * @val: square root
- * @Return: int
+ * Return: int
 */
+
 int square(int n, int val);
 int _sqrt_recursion(int n)
 {
-	return (square(n, 1));
+	return (squar(n, 1));
 }
 
 /**
