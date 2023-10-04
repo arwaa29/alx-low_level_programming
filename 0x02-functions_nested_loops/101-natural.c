c1497a0f6f2e5a7282c9816a4115c440579fe134
arwaa29 <arwahesham73@gmail.com>
2023-10-04 14:26:24 +0300

    alx 3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main - entry point.
+ *
+ * Description: computes and prints sum of all numbers that divisble by 3 or 5
+ *              below 1024.
+ *
+ * Return: 0 always (success).
+*/
+
+int main(void)
+{
+	int sum = 0, num;
+
+	for (num = 0 ; num < 1024 ; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d", num);
+
+	return (0);
+}
