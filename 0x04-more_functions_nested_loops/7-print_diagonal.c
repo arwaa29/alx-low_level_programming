4c06a3f0dc44e2c775fb7e8a8119d870b81ffc01
arwaa29 <arwahesham73@gmail.com>
2023-10-10 04:15:43 +0300

    alx 2

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,17 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int diagonal, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (postn = 1; postn <= n; postn++)
+		for (diagonal = 0; diagonal < n; diagonal++)
 		{
-			for (space = 1; space <= postn; space++)
+			for (space = 0; space < diagonal; space++)
 				_putchar(' ');
+
 			_putchar(92); /*is equal to '\' char*/
 			_putchar('\n');
 		}
