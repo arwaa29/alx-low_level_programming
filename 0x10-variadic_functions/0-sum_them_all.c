ffc4b342ea078981b499fb9c2a66bc7ae7f7ae29
arwaa29 <arwahesham73@gmail.com>
2023-10-13 02:25:37 +0300

    alx 10c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+
+/**
+ * sum_them_all - sums variable arguments
+ *
+ * @n: it is  the number of arguments
+ * @...: it is the integers to sum
+ *
+ * Return: the integer sum
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int x = 0, i = n;
+	va_list ap;
+
+	if (!n)
+		return (0);
+	va_start(ap, n);
+	while (i--)
+		x += va_arg(ap, int);
+	va_end(ap);
+	return (s);
+}
