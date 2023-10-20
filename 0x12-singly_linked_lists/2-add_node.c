9ca50613473e71929d46db53015096177623c3b9
arwaa29 <arwahesham73@gmail.com>
2023-10-20 03:31:36 +0300

    alx 12c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_head = malloc(sizeof(list_t));
 
 	if (!head || !new_head)
-		return (null);
+		return (NULL);
 	if (str)
 	{
 		new_head->str = strdup(str);
