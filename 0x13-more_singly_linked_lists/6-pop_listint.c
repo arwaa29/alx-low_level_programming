12913b27ead174d94f8334177d0ae0454e7def66
arwaa29 <arwahesham73@gmail.com>
2023-10-25 04:59:33 +0300

    alx 13c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - it is the pops head node of the list
+ * @head: address of pointer to the first node
+ *
+ * Return: value of popped node
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (!head || !*head)
+		return (0);
+	node = (*head)->nextn;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
