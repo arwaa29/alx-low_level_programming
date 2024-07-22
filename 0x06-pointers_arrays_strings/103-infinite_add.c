5a489f6fed3b4d04b9f9d146052aec4f1e45f58f
arwaa29 <arwahesham73@gmail.com>
2024-07-22 03:25:28 +0300

    alx 6

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * infinite_add: function add two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r : buffer where the result will be stored in it
+ * @size_r: result length(buffer size)
+ * Return : sum of two numbers
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+    /*local variable declaration*/
+	int i = 0, j = 0;
+	int large, firstDigit, secondDigit, carryout = 0;
+	while (n1[i] != NULL)
+		i++;
+
+	while (n2[j] != NULL)
+		j++;
+
+	if (i > j)
+		large = i;
+	else
+		large = j;
+
+	if (large + 1 > size_r)
+		return (0);
+
+	r[large] = '\0';
+	for (int k = large - 1 ; k >= 0 ; k--)
+	{
+		i--;
+		j--;
+
+		if (i >= 0)
+			firstDigit = n1[i] - 48;
+		else
+			firstDigit = 0;
+
+		if (j >= 0)
+		{
+			secondDigit = n2[j] - 48;
+		}
+		else
+			secondDigit = 0;
+
+		r[k] = (firstDigit + secondDigit + carryout) % 10 + 48;
+		carryout = (firstDigit + secondDigit + carryout) / 10;
+	}
+
+	if (carryout == 1)
+	{
+		r[large + 1] = '\0';
+		if (large + 2 > size_r)
+			return (0);
+
+		while (large-- > 0)
+			r[large + 1] = r[large];
+		r[0] = carryout + '0';
+	}
+	return (r);
+}
