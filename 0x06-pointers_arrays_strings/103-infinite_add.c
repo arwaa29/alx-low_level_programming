4b955c31da1da6a9b1ed0795f1530d9d29debac7
arwaa29 <arwahesham73@gmail.com>
2024-07-24 02:58:53 +0300

    alx 6

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -52,7 +52,7 @@ void shift_result_right(char *r, int large)
  * @carryout: The carry out value
  */
 
-char  handle_carry(char *r, int *large, int size_r, int carryout)
+char *handle_carry(char *r, int *large, int size_r, int carryout)
 {
 	r[*large + 1] = '\0';
 	if (*large + 2 > size_r)
